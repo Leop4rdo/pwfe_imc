b621c2b61eac0c139ed7c35867a8772a333a5f82
LEONARDO BOMFIM ANTUNES <21276366@CFP127ALUNO.LOCAL>
2022-01-26 16:59:00 -0300

    commit

--- imc.js
+++ imc.js
@@ -5,13 +5,11 @@ document.querySelector("button#calcular").addEventListener("click", () => {
     const altura = document.querySelector("input#altura").value;
     const peso = document.querySelector("input#peso").value;
     const resultado = document.querySelector("div#resultado");
-    
-    console.log(validarCampos())
 
     if(validarCampos()) {
         const imc = calcularIMC(peso, altura);
     
-        let categoria = classificarIMC();
+        let categoria = classificarIMC(imc);
 
         resultado.textContent = `${nome} seu imc é de ${imc.toFixed(2)} e você esta ${categoria}`;
     } else {
@@ -26,23 +24,35 @@ function validarCampos() {
 }
 
 function calcularIMC(peso, altura){
-    return peso / altura**2;
+    return peso / (altura * altura);
 }
 
 function classificarIMC(imc) {
+    let aviso = document.querySelector("span");
+
     let categoria;
+
+    console.log(imc)
     if (imc < 18.5){
         categoria = "abaixo do peso";
+        aviso.className = "invisible";
     } else if (imc < 25) {
         categoria = "com peso normal";
+        aviso.textContent = "Parabéns!"
+        aviso.className = "verde";
     } else if (imc < 30) {
         categoria = "acima do peso";
+        aviso.className = "invisible";
     } else if (imc < 35) {
         categoria = "com obesidade grau I";
+        aviso.className = "invisible";
     } else if (imc < 40) { 
         categoria = "com obesidade grau II";
+        aviso.className = "invisible";
     } else {
         categoria = "com obesidade mórbida";
+        aviso.textContent = "Cuidado!"
+        aviso.className = "vermelho";
     }
 
     return categoria;
