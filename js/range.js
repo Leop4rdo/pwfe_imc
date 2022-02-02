4e829e1fb7f0c55b90585d0f5e1f8f59b6eda50a
LEONARDO BOMFIM ANTUNES <21276366@CFP127ALUNO.LOCAL>
2022-02-02 16:13:54 -0300

    mudando estilo do slider

--- js/range.js
+++ js/range.js
@@ -4,11 +4,8 @@ const iniciarRange = (id) => {
     const mostrarValorSlider = () => {
         const slider = document.querySelector(`.slider#${id}`);
         const span = document.querySelector(`span#valor_${id}`);
-        
-        let unidade = (id === "peso") ? "kg" : "m";
-        
 
-        span.textContent = `${slider.value}${unidade}`;
+        span.textContent = `${slider.value}${slider.dataset.unidade}`;
     }
 
     document.querySelector(`.slider#${id}`).addEventListener("input", mostrarValorSlider);
